9e71bd9114a0610855752ca7880ab59d170555a1
Kos_hatz <Konstantinos Hatzistamatis>
2018-07-10 23:14:34 +0300

    Fixed sending mail functionality.

--- src/main/java/intraholics/ticketmonster/Mail/EmailSessionBean.java
+++ src/main/java/intraholics/ticketmonster/Mail/EmailSessionBean.java
@@ -7,6 +7,18 @@ package intraholics.ticketmonster.Mail;
 
 
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.mail.*;
@@ -14,29 +26,46 @@ import javax.mail.internet.MimeMessage;
 import javax.mail.internet.InternetAddress;
 
 
-/**
- *
- * @author Kostis
- */
 @Stateless
-
 public class EmailSessionBean{
-    @Resource(name ="java:jboss/mail/gmail")
-    private Session session;
+	
+	public void SendMail(String email, String password) throws Exception {
 
-  
-    
-    public void sendEmail(String to,String subject,String body){
-        MimeMessage message = new MimeMessage(session);
-try {
-    message.setRecipient(Message.RecipientType.TO, InternetAddress.parse(to)[0]);
-    message.setSubject(subject);
-    message.setText(body);
-    Transport.send(message);
-} catch (MessagingException ex) {
-    ex.printStackTrace();
-}
-    }
-}
+		EmailSessionBean http = new EmailSessionBean();
 
-    
+		System.out.println("Testing 1 - Send Http GET request");
+		http.sendGet(email,password);
+	}
+
+	// HTTP GET request
+	public void sendGet(String Email, String Password) throws Exception {
+
+		String url = "http://localhost:60109/api/values/"+Email+"&"+Password;
+		
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+	
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+
+	}
+	
+}
